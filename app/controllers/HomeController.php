1878decd3fe880899b3ef9053011c91070c7e2c0
rachidtahri <rachid.tahri.2025@gmail.com>
2025-10-23 11:09:10 +0100

    Delete app/controllers directory

--- app/controllers/HomeController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class HomeController extends Controller {
-    public function index() {
-        $model = new HomeModel();
-        $data = $model->getData();
-        include '../app/views/home.php';
-    }
-}
