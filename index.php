899a1c50c2442b7ef2da534a46514735d52526db
TahrinetSurface <rachid.tahri.2015@gmail.com>
2025-10-23 11:49:05 +0100

    changeTitle

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Nejjari Abdelouahab</title>
+    <title>Formation Academia Raqmya</title>
 </head>
 <body>
     <h1> ceci est un test distant</h1>
